c1064cae06fe65579c07972ac04e799a491d50e9
Lucacontr <lucacontrasto@gmail.com>
2023-05-23 23:00:32 +0200

    added 3 more extensions to compress the file

--- src/main/demo/src/main/java/com/example/demo/MyController.java
+++ src/main/demo/src/main/java/com/example/demo/MyController.java
@@ -18,6 +18,10 @@
  */
 package com.example.demo;
 
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -34,7 +38,7 @@ import java.io.*;
 @Controller
 public class MyController {
     //codice per gestire la compressione tramite la libreria
-    @PostMapping(value = "/compress")
+    @PostMapping(value = "/compress/tar")
     public  ResponseEntity<Resource> submit(@RequestParam("fileToCompress") MultipartFile file) throws IOException {
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             try (TarArchiveOutputStream tarOutput = new TarArchiveOutputStream(baos)) {
@@ -56,6 +60,73 @@ public class MyController {
                     .headers(headers)
                     .body(resource);
 
+        }
+    }
+
+    @PostMapping("/compress/zip")
+    public ResponseEntity<ByteArrayResource> compressFileToZip(@RequestParam("fileToCompress") MultipartFile file) throws IOException {
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            try (ZipArchiveOutputStream zipOutput = new ZipArchiveOutputStream(baos)) {
+                ZipArchiveEntry entry = new ZipArchiveEntry(file.getOriginalFilename());
+                entry.setSize(file.getSize());
+                zipOutput.putArchiveEntry(entry);
+                InputStream inputStream = file.getInputStream();
+                StreamUtils.copy(inputStream, zipOutput);
+                inputStream.close();
+                zipOutput.closeArchiveEntry();
+            }
+            byte[] compressedBytes = baos.toByteArray();
+            ByteArrayResource resource = new ByteArrayResource(compressedBytes);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=file.zip");
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
+            headers.add(HttpHeaders.CONTENT_LENGTH, Integer.toString(compressedBytes.length));
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(resource);
+        }
+    }
+
+    @PostMapping("/compress/bzip2")
+    public ResponseEntity<ByteArrayResource> compressFileToBzip2(@RequestParam("fileToCompress") MultipartFile file) throws IOException {
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            try (BZip2CompressorOutputStream bzip2Output = new BZip2CompressorOutputStream(baos)) {
+                InputStream inputStream = file.getInputStream();
+                StreamUtils.copy(inputStream, bzip2Output);
+                inputStream.close();
+            }
+            byte[] compressedBytes = baos.toByteArray();
+            ByteArrayResource resource = new ByteArrayResource(compressedBytes);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=file.bz2");
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
+            headers.add(HttpHeaders.CONTENT_LENGTH, Integer.toString(compressedBytes.length));
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(resource);
+        }
+    }
+
+    @PostMapping("/compress/gzip")
+    public ResponseEntity<ByteArrayResource> compressFileToGzip(@RequestParam("fileToCompress") MultipartFile file) throws IOException {
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            try (GzipCompressorOutputStream gzipOutput = new GzipCompressorOutputStream(baos)) {
+                InputStream inputStream = file.getInputStream();
+                StreamUtils.copy(inputStream, gzipOutput);
+                inputStream.close();
+            }
+            byte[] compressedBytes = baos.toByteArray();
+            ByteArrayResource resource = new ByteArrayResource(compressedBytes);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=file.gz");
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
+            headers.add(HttpHeaders.CONTENT_LENGTH, Integer.toString(compressedBytes.length));
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(resource);
         }
     }
 }
